2f30cff938e05756966402e4d054381bd96c854f
WaghmareAkanksha <akankshawaghmare.aw@gmail.com>
2022-08-08 09:11:50 +0530

    Emp01:Citizen class addedd

---
+++ src/main/java/com/Infosys/controller/CitizenController.java
@@ -0,0 +1,31 @@
+package com.Infosys.controller;
+
+public class CitizenController {
+	
+	private Integer cid;
+	private String cname;
+	private String caddress;
+	
+	public Integer getCid() {
+		return cid;
+	}
+	public void setCid(Integer cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public String getCaddress() {
+		return caddress;
+	}
+	public void setCaddress(String caddress) {
+		this.caddress = caddress;
+	}
+	@Override
+	public String toString() {
+		return "CitizenController [cid=" + cid + ", cname=" + cname + ", caddress=" + caddress + "]";
+	}
+}
